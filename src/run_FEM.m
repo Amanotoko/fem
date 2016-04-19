930d2da93c4a8458ad75f8ab0a10111ee61c6ed7
Kai He <khe004@ucr.edu>
2016-04-19 09:17:55 -0700

    example

--- src/run_FEM.m
+++ src/run_FEM.m
@@ -1,5 +1,5 @@
-mf = '../example/Net109.msh';
-bf = '../example/Net109.cond';
-pf = '../example/ppNet109.msh';
+mf = '../example/A3_GPIO.msh';
+bf = '../example/A3_GPIO.cond';
+pf = '../example/ppA3_GPIO.msh';
 FEM_current_density(mf, bf, pf);
 
