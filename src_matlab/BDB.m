780f000203fdb202807f603e03cb6f4b4e67c925
Sheldon Tan <stan@dhcp45.engr.ucr.edu>
2016-06-17 16:58:50 -0700

    add comments

--- src_matlab/BDB.m
+++ src_matlab/BDB.m
@@ -8,13 +8,16 @@ LMtl = size(Material,1);
 
 for n = 1:LMtl,
     D = [Material(n,2) 0 0; 0 Material(n,2) 0; 0 0 Material(n,2)];
+    % get the volumn element position (material index = 56)
     Dpos = find(TetraEle(:,4)==Material(n,1));
     LDpos = length(Dpos);
 
     for k = 1:LDpos,
         % see eq(3.261) in the book by R. Lewis pp. 91
         Node = TetraEle(Dpos(k),6:9);
-        nodecor = NodeCor(Node,2:4);
+        nodecor = NodeCor(Node,2:4); 
+        % V is the volumn of the elementm B is the gradient matrix (first
+        % order derivative of the function.
         [V B] = BMat(nodecor);
         BDBm = B'*D*B*V;
         KBDB(Node,Node) = KBDB(Node,Node)+ BDBm;
